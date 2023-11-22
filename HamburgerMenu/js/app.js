b974c4ae7ebf11bb39c1f7af1c4334b48df7efaa
Wes <Wes.Freeman17@gmail.com>
2023-11-22 11:01:33 -0600

    git push not behaving

---
+++ HamburgerMenu/js/app.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.style.display === "block" ? navLinks.style.display = "none" : navLinks.style.display = "block";
+   }
